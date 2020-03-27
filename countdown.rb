5fae6dc0ebbbd10672d9e64bfa912a363fe2b416
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-03-27 03:26:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  until number == 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
